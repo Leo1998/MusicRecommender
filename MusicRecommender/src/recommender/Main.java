7686e9fbdf06ed2c018087298953d7392cf399c4
Leonard Fricke <fricke@ananke.cs.tu-dortmund.de>
2021-01-04 17:45:09 +0100

    stufff

--- MusicRecommender/src/recommender/Main.java
+++ MusicRecommender/src/recommender/Main.java
@@ -1,9 +1,19 @@
 package recommender;
 
+import java.io.File;
+import java.io.IOException;
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Music:)");
+
+		try {
+			Process p = Runtime.getRuntime().exec("./amuse.sh", null, new File("../"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
